b7a700db865e57f5ab5d6933efccd26f459789b5
design <design@designteam-macbook.local>
2015-09-18 14:18:51 +0900

    Initial Commit

---
+++ admin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  admin
+//
+//  Created by design on 2015. 9. 18..
+//  Copyright (c) 2015년 design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
